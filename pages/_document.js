24f0808eba5e0ce0ce0b76623b6fe9b211950ca0
Sean Gibson <sean.gibson@kodaro.com>
2018-07-16 14:20:56 -0700

    updates _document.js to fix missing return

--- pages/_document.js
+++ pages/_document.js
@@ -2,14 +2,16 @@ import Document, { Head, Main, NextScript } from 'next/document';
 
 export default class CustomDocument extends Document {
   render() {
-    <html>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </html>;
+    return (
+      <html>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
   }
 }
