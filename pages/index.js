5072acce06d9d2037a5cbfa88b08a4bde72ba024
Sean Gibson <sean.gibson@kodaro.com>
2018-07-15 19:23:49 -0700

    adds index page

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import { H1, H2, H3, H4 } from '../components/headings';
+import Page from '../components/Page';
+
+export default () => (
+  <Page title="Next starter repo">
+    <H1> Welcome to Next Starter</H1>
+    <H2> Welcome to Next Starter</H2>
+    <H3> Welcome to Next Starter</H3>
+    <H4> Welcome to Next Starter</H4>
+  </Page>
+);
